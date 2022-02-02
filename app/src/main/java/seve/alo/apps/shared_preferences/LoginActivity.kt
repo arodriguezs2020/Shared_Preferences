1d50022276d10299b5eef457737ffcdf8b65f5c4
arodriguezs2020 <dam16.2020@gmail.com>
2022-02-02 13:59:51 +0100

    Aun tiene fallos

--- app/src/main/java/seve/alo/apps/shared_preferences/LoginActivity.kt
+++ app/src/main/java/seve/alo/apps/shared_preferences/LoginActivity.kt
@@ -1,38 +1,63 @@
 package seve.alo.apps.shared_preferences
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.text.TextUtils
 import android.util.Patterns
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Switch
 import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 
 class LoginActivity : AppCompatActivity() {
+
+    var preferencias : SharedPreferences?= null
+    var switchRecordar: Switch?= null
+    var editTextEmail : EditText?= null
+    var editTextPass : EditText?= null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val btnLogin = findViewById<Button>(R.id.btnLogin)
-        val editTextEmail = findViewById<EditText>(R.id.editTextEmail)
-        val editTextPass = findViewById<EditText>(R.id.editTextPass)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
+        switchRecordar = findViewById<Switch>(R.id.switchRecordar)
+
+        // Modo en que se crea el archivo, si no se necesita multiples Preferencias usamos las Shared por defecto
+        preferencias = PreferenceManager.getDefaultSharedPreferences(this)
+
+        // Si se necesitan multiples preferencias
+        preferencias = getSharedPreferences("Preferencias", Context.MODE_PRIVATE)
+
+        ponerPreferenciasSiExisten()
+
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+        editTextEmail = findViewById(R.id.editTextEmail)
+        editTextPass = findViewById(R.id.editTextPass)
 
         btnLogin.setOnClickListener {
-            val email = editTextEmail.toString()
-            val pass = editTextPass.toString()
+            val email = editTextEmail!!.text.toString()
+            val pass = editTextPass!!.text.toString()
 
             if (logeo(email, pass)) {
                 val intent = Intent(this, MainActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
                 startActivity(intent)
+                guardarPreferencias(email, pass)
             }
         }
-
     }
     
     fun validarEmail(email: String) : Boolean {
-        return !TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches()
+        return !email.isNullOrEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()
     }
     
     fun validarPass(pass: String) : Boolean {
@@ -48,6 +73,26 @@ class LoginActivity : AppCompatActivity() {
             return false
         } else {
             return true
+        }
+    }
+
+    fun guardarPreferencias(email: String, pass: String) {
+        if (switchRecordar!!.isChecked) {
+            preferencias!!.edit()
+                .putString("email", email)
+                .putString("pass", pass)
+                .apply()
+        }
+    }
+
+    fun ponerPreferenciasSiExisten() {
+        val email = preferencias?.getString("email", "")
+        val pass = preferencias?.getString("pass", "")
+
+        if (!email.isNullOrEmpty() && !pass.isNullOrEmpty()) {
+            editTextEmail!!.setText(email)
+            editTextPass!!.setText(pass)
+            switchRecordar!!.isChecked = true
         }
     }
 }
--- app/src/main/java/seve/alo/apps/shared_preferences/MainActivity.kt
+++ app/src/main/java/seve/alo/apps/shared_preferences/MainActivity.kt
@@ -1,11 +1,59 @@
 package seve.alo.apps.shared_preferences
 
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import com.google.android.material.appbar.CollapsingToolbarLayout
 
 class MainActivity : AppCompatActivity() {
+
+    var toolbarColaps : CollapsingToolbarLayout? = null
+
+    var preferencias : SharedPreferences? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toolbarColaps = findViewById<CollapsingToolbarLayout>(R.id.collapsing_toolbar)
+        toolbarColaps!!.title = "Beneficios de usar Kotlin"
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        preferencias = PreferenceManager.getDefaultSharedPreferences(this)
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean  = when(item.itemId){
+
+        R.id.menuSalir -> {
+            val intent = Intent(this@MainActivity, LoginActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            startActivity(intent)
+            true
+        }
+        R.id.menuOlvidar -> {
+            preferencias!!.edit().clear().apply()
+            val intent = Intent(this@MainActivity, LoginActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            startActivity(intent)
+            true
+        }
+        else ->false
+    }
+
+
 }
